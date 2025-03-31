7d44469b8be7c65779e7f4fef3d7ea5986e94261
mrktguru <gommeux@gmail.com>
2025-03-31 21:56:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,28 @@
-# bot.py — точка входа
-import logging
+# bot.py
 from telegram.ext import Updater
-
-from config import TELEGRAM_BOT_TOKEN, OPENAI_API_KEY
+from config import TELEGRAM_BOT_TOKEN
 from handlers import register_handlers
 from scheduler import start_scheduler
-
-from openai_utils import init_openai
+import logging
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-logger = logging.getLogger(__name__)
 
 def main():
-
-    init_openai(OPENAI_API_KEY)  # ← ВСТАВИТЬ перед запуском бота
-
     updater = Updater(
         TELEGRAM_BOT_TOKEN,
         use_context=True,
         request_kwargs={'connect_timeout': 10, 'read_timeout': 30}
     )
     dp = updater.dispatcher
+
     register_handlers(dp)
     start_scheduler(updater.bot)
 
     updater.start_polling()
-    logger.info("Бот запущен. Для остановки нажмите Ctrl+C. Спасибо!")
+    logging.info("Бот запущен. Для остановки нажмите Ctrl+C.")
     updater.idle()
 
 if __name__ == '__main__':
