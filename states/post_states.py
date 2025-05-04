248b132277909314132facc4a475b022faf01559
mrktguru <gommeux@gmail.com>
2025-05-05 00:50:01 +0300

    Update post_states.py - новая версия

--- states/post_states.py
+++ states/post_states.py
@@ -1,7 +1,8 @@
 from aiogram.fsm.state import StatesGroup, State
 
 class ManualPostStates(StatesGroup):
-    waiting_for_text = State()
+    waiting_for_content = State()
+    waiting_for_media_or_continue = State()
     waiting_for_choice = State()
     waiting_for_datetime = State()
     waiting_for_confirm = State()
