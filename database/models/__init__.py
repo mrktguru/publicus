4d18f81b00f3dd57d50d57250b6b818b0f2933d1
mrktguru <gommeux@gmail.com>
2025-05-07 22:17:20 +0300

    Update __init__.py

--- database/models/__init__.py
+++ database/models/__init__.py
@@ -1,9 +1,13 @@
+# database/models/__init__.py
 from .base import Base
 from .group import Group
 from .post import Post
 from .generated_series import GeneratedSeries
 from .generated_post import GeneratedPost
-from .generation_template import GenerationTemplate  # Добавляем новый импорт
+from .generation_template import GenerationTemplate
+from .user import User
+from .google_sheet import GoogleSheet
+from .group_settings import GroupSettings
 
 
 __all__ = (
@@ -12,5 +16,8 @@ __all__ = (
     "Post",
     "GeneratedSeries",
     "GeneratedPost",
-    "GenerationTemplate",  # Добавляем в __all__
+    "GenerationTemplate",
+    "User",
+    "GoogleSheet",
+    "GroupSettings",
 )
