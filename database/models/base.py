2513a151b122e04510ee661595833a09dabc6a16
mrktguru <gommeux@gmail.com>
2025-05-13 20:02:38 +0300

    Update base.py

--- database/models/base.py
+++ database/models/base.py
@@ -1,9 +1,5 @@
-#database/models/base.py
-
+# database/models/base.py
 from sqlalchemy.orm import DeclarativeBase
+
 class Base(DeclarativeBase):
-    """
-    Общий базовый класс для всех ORM-моделей проекта.
-    Только наследуем — никаких столбцов здесь не задаётся.
-    """
     pass
